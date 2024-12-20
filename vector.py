e549affe6dfdbdb77b9958301804b930057464ea
Pedro Baptista <pedrolk2012@gmail.com>
2024-12-19 21:02:45 -0300

    feat: implement vector class

--- vector.py
+++ vector.py
@@ -1,3 +1,4 @@
+import math
 class Vector:
     """
     Representa um vetor em um espaço tridimensional.
@@ -14,6 +15,35 @@ class Vector:
 
     def __str__(self):
         return f"({self.x}, {self.y}, {self.z})"
-    
-    # Implemente os métodos de vetores aqui
-    
+
+    def __add__(self, vector):
+        return Vector(self.x + vector.x, self.y + vector.y, self.z + vector.z)
+
+    def __sub__(self, vector):
+        return Vector(self.x - vector.x, self.y - vector.y, self.z - vector.z)
+
+    def magnitude(self):
+        return (self.x**2 + self.y**2 + self.z**2) ** 0.5
+
+    def normalize(self):
+        return Vector(self.x / self.magnitude(), self.y / self.magnitude(), self.z / self.magnitude())
+
+    def cross_product(self, vector):
+        return Vector(
+            self.y * vector.z - self.z * vector.y,
+            self.z * vector.x - self.x * vector.z,
+            self.x * vector.y - self.y * vector.x
+        )
+
+    def normalize(self):
+        return Vector(
+            self.x / self.magnitude(),
+            self.y / self.magnitude(),
+            self.z / self.magnitude()
+        )
+
+    def angle(self, vector):
+        return math.acos((self * vector) / (self.magnitude() * vector.magnitude()))
+
+
+
