6ac32fbc5ad43cc68dd4432a1f402ff3ad2c0a43
Lucas Henrique <hns.lucass@gmail.com>
2024-12-22 17:52:40 -0300

    feat(main): add main code to test

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from obj_reader import ObjReader
 from camera import Camera
+from renderer import Renderer
 from point import Point
 from vector import Vector
+from sphere import Sphere
+from plane import Plane
+
 # Divirtam-se :)
 
 def main():
-    camera = Camera(camera_point=Point(0, 0, 0), target_point=Point(0, 0, 1), vector_up=Vector(0, 1, 0), width=300, height=300)
+    # Configurações de câmera
+    camera = Camera(
+        camera_point=Point(0, 0, -5), 
+        target_point=Point(0, 0, 0), 
+        vector_up=Vector(0, 1, 0), 
+        hres=800, 
+        vres=600
+    )
     print(camera)
-    camera.ray_casting([], 2000)
 
+    # Criar objetos
+    sphere = Sphere(center=Point(0,0,5), radius=1, color=(255,0,0))
+    plane = Plane(point=Point(0,-1,0), normal=Vector(0,1,0), color=(0,255,0))
+
+    # Renderizar a cena
+    renderer = Renderer(camera, [sphere, plane], hres=800, vres=600)
+    renderer.render()
 
 
 if __name__ == "__main__":
